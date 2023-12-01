54cba2a4766fe27558d003afd3acf4a4c15e4325
JohanGM06 <desarrollo@cbit-online.com>
2023-11-30 21:34:01 -0500

    Se agregaron cosas xd

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,16 +1,21 @@
 <?PHP
 class DataBase{
     public static function connection(){
-        $hostname = "localhost:3306";
+                    
+        $hostname = "sgaproyecto2023.mysql.database.azure.com";
         $port = "3306";
-        $database = "SGA2";
-        $username = "root";
-        $password = "";
-
-        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+        $database = "sga2";
+        $username = "zac";
+        $password = "Qwertyuiop123456.";
+        $options = array(
+            PDO::MYSQL_ATTR_SSL_CA => 'assets/DigiCertGlobalRootCA.crt.pem'
+        );
+        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
+     }
+    
   
-    }
 }
+
 ?>
