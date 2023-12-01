b9d3f59ed8ba44826963024cded7df80854ff2ad
JohanGM06 <desarrollo@cbit-online.com>
2023-11-30 20:43:37 -0500

    Se cambio la DataBase

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,18 +1,34 @@
 <?PHP
 class DataBase
 {
-    public static function connection()
-    {
-        $hostname = "localhost:3306";
+    
+    public static function connection(){            
+        $hostname = "sgaproyecto2023.mysql.database.azure.com";
         $port = "3306";
-        $database = "SGA2";
-        $username = "root";
-        $password = "";
-
-        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+        $database = "sgaproyecto2023";
+        $username = "zac";
+        $password = "Qwertyuiop123456.";
+        $options = array(
+            PDO::MYSQL_ATTR_SSL_CA => 'assets/DigiCertGlobalRootCA.crt.pem'
+        );
+        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
-
     }
 }
+
+    // public static function connection()
+    // {
+    //     $hostname = "localhost:3306";
+    //     $port = "3306";
+    //     $database = "SGA2";
+    //     $username = "root";
+    //     $password = "";
+
+    //     $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+    //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     return $pdo;
+
+    // }
+
 ?>
